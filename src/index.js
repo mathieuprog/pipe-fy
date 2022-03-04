8c4c348d033c43744cbbfc016f4dd1e261574ce7
mathieuprog <5883963+mathieuprog@users.noreply.github.com>
2022-03-04 11:34:17 +0100

    Add pipeAsync

--- src/index.js
+++ src/index.js
@@ -4,13 +4,28 @@ const pipe = (...functions) => {
       return fun(accumulator);
     }, initialValue);
   }
-}
+};
+
+const pipeAsync = (...functions) => {
+  return (initialValue) => {
+    return doPipeAsync(functions, initialValue);
+  };
+};
+
+const doPipeAsync = async (functions, accumulator) => {
+  accumulator = await functions.shift()(accumulator);
+
+  return (functions.length > 0)
+    ? doPipeAsync(functions, accumulator)
+    : accumulator;
+};
 
 const pipefy = (fun, ...args) => {
   return (accumulator) => fun(accumulator, ...args);
-}
+};
 
 export {
   pipe,
+  pipeAsync,
   pipefy
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,9 @@
-import { pipe, pipefy } from './index';
+import { pipe, pipeAsync, pipefy } from './index';
 
-test('setNestedProp', () => {
+test('pipe', () => {
   const getName = (person) => person.name;
   const uppercase = (string) => string.toUpperCase();
-  const repeat = (string, n) => string.repeat(n);
+  const repeat = (string, n) => (string + ' ').repeat(n).trimEnd();
 
   const person = { name: 'Mathieu' };
 
@@ -14,5 +14,24 @@ test('setNestedProp', () => {
       pipefy(repeat, 2)
     )(person);
 
-  expect(result).toBe('MATHIEUMATHIEU');
+  expect(result).toBe('MATHIEU MATHIEU');
+});
+
+test('pipeAsync', async () => {
+  const getName = (person) => person.name;
+  const uppercase = (string) => string.toUpperCase();
+  const repeat = (string, n) => (string + ' ').repeat(n).trimEnd();
+
+  const uppercaseAfterTimeout = (string) => new Promise(resolve => setTimeout(() => resolve(uppercase(string)), 50));
+
+  const person = { name: 'Mathieu' };
+
+  const result =
+    await pipeAsync(
+      getName,
+      uppercaseAfterTimeout,
+      pipefy(repeat, 2)
+    )(person);
+
+  expect(result).toBe('MATHIEU MATHIEU');
 });
